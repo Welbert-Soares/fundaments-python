3e871425b335aef88896a0aa439234736110b650
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2025-05-19 18:37:42 -0300

    Adiciona exemplo de uso de loops for com listas e dicionários em Python

---
+++ modulo-1/for.py
@@ -0,0 +1,14 @@
+# for 
+
+list = [1, 2, 3, 4, 5]
+total = 0
+for i in list:
+  total += i
+
+print(total)
+
+
+peaple = {"name": "Lucas", "age": 18, "height": 1.80}
+
+for key, value in peaple.items():
+  print(f"{key}: {value}")
